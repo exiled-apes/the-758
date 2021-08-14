1f60e27cf05d45ee2b8e05cb204a311ac32ff25d
Levi Cook <levicook@gmail.com>
2021-08-14 15:23:19 -0600

    some cli boilerplate

---
+++ README.md
@@ -0,0 +1,7 @@
+# the-758
+
+This is a command line oriented block explorer that identifies the
+original 758 degen apes.
+
+The goal if this program is simple -- traverse blockchain data, using an
+RPC server and produce a canonical list of token addresses.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use gumdrop::Options;
+
+#[derive(Debug, Options)]
+struct AppOptions {
+    #[options(help = "Solana rpc server url", default_expr = "default_rpc_url()")]
+    rpc_url: String,
+}
+
 fn main() {
     println!("Hello, apes!");
+
+    let app_options = AppOptions::parse_args_default_or_exit();
+
+    let rpc_url = app_options.rpc_url;
+
+    println!("{}", rpc_url)
+}
+
+fn default_rpc_url() -> String {
+    "https://api.mainnet-beta.solana.com".to_owned()
 }
